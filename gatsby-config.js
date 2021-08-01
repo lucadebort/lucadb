c3262cb1cf1fc1d1e9a7c526e51685ff0975e1e3
Luca De Bortoli <luca@etnograph.com>
2021-08-01 12:55:57 +0200

    gatsby-plugin-manifest e favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,19 @@ module.exports = {
     {
       resolve: 'gatsby-source-medium',
       options: {
-        username: '@lucadebort', // Medium user name
+        username: '@lucadebort',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
       },
     },
   ],
