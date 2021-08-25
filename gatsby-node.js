71bdedfa1f750ab071ebdb9d3b04c3eeea39653c
lucadebort <34425644+lucadebort@users.noreply.github.com>
2021-08-25 17:56:41 +0200

    boh

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       // In your blog post template's graphql query, you can use pagePath
       // as a GraphQL variable to query for data from the markdown file.
       context: {
-        path: node.frontmatter.path,
         id: node.id
       },
     })
